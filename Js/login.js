c7d99ea4a80613d0f553e7410f2138de89967358
JoeyAlpha5 <35291886+JoeyAlpha5@users.noreply.github.com>
2018-09-29 20:38:34 +0200

    update

--- Js/login.js
+++ Js/login.js
@@ -5,6 +5,15 @@ firebase.auth().onAuthStateChanged(function(user) {
         document.location = 'home.html';
       }else if(window.location.href.includes("main.html")) {
         var userId = firebase.auth().currentUser.uid;
+        var database = firebase.database().ref();
+
+        database.on('child_added', (data)=> {
+          userId.on('value', (user)=>{
+            var userType = data.child(user.val()).child("Type").val();
+            console.log(userType);
+          });
+        });
+
       }
         
     } else {
