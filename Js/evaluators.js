b0fe374fe0273448a2f92bbc63f1192933418290
Mandlenkosi <Ndukuzempi@gmail.com>
2018-09-29 22:49:05 +0200

    Fixed issues

--- Js/evaluators.js
+++ Js/evaluators.js
@@ -1,24 +1,13 @@
-function addEvaluators(guardianId)
+getEvaluators();
+function getEvaluators()
 {    
-    loadfirebase();
-
-    var database = firebase.database().ref().child("guardian").child("100");
+   var database = firebase.database().ref('Valuator');
     //var userId = getEvaluatorsByGuardianId(guardianId)
-
-    database.on('value', function(data){
-        var name = data.child("name").val();
-        console.log(name);
+    var arrObj = [];
+    database.on('child_added', function(data){
+        var list = data.val();
+        arrObj.push(list)      
+           
+        console.log(data.key);
      });
-}
-function loadfirebase()
-{
-    var config = {
-        apiKey: "AIzaSyDgN9tdNaJh0SFe71rZXT5zC4ixLWfSuQ0",
-        authDomain: "proj-3e49b.firebaseapp.com",
-        databaseURL: "https://proj-3e49b.firebaseio.com",
-        projectId: "proj-3e49b",
-        storageBucket: "proj-3e49b.appspot.com",
-        messagingSenderId: "512818315638"
-    };
-    firebase.initializeApp(config);
 }
